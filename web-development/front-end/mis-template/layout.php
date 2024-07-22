e67c586fa89df8ba66c5eb7a252952a310e145cb
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-07-22 16:14:45 +0530

    Work on new task module Fixes #5

---
+++ web-development/front-end/mis-template/layout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <?php include 'header.php' ?>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+    <?php include 'footer.php' ?>
+    </footer>
+</body>
+
+</html>
