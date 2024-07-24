a8ec9f26eab93d5244df55b194d320b2beab4aea
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-07-24 16:06:46 +0530

    Work on new task module Fixes #5

---
+++ web-development/front-end/mis-template/dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - MIS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <?php include 'header.php' ?>
+    </header>
+    <main>
+        <div class="card-container">
+            <div class="card">
+                Div 1
+            </div>
+            <div class="card">
+                Div 2
+            </div>
+            <div class="card">
+                Div 3
+            </div>
+            <div class="card">
+                Div 4
+            </div>
+        </div>
+    </main>
+    <footer>
+        <?php include 'footer.php' ?>
+    </footer>
+</body>
+
+</html>
