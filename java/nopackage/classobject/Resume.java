fba880a94073afa2060bd5e13de08c7798b76a45
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-03 15:52:12 +0530

    Work on new task module Fixes #5

--- java/nopackage/classobject/Resume.java
+++ java/nopackage/classobject/Resume.java
@@ -6,6 +6,7 @@ public class Resume {
     private String mobile;
     private String _email;
 
+    
     public int getId() {
         return id;
     }
---
+++ projects/todoapp/database/create-database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE todoapp_2404;
