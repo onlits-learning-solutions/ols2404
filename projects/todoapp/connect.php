7ad879759d6d558d3e1deea120faa53b994ba9d9
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-16 16:25:08 +0530

    Work on new task module Fixes #5

--- projects/todoapp/connect.php
+++ projects/todoapp/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('localhost', 'ols2404', 'ols2404', 'todoapp');
+$connection = mysqli_connect('localhost', 'ols2404', 'ols2404', 'todoapp_2404');
 
 if (!$connection) {
     die(mysqli_connect_error());
--- projects/todoapp/myhome.php
+++ projects/todoapp/myhome.php
@@ -19,6 +19,7 @@ if (mysqli_num_rows($result) > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TodoApp - MyHome</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
