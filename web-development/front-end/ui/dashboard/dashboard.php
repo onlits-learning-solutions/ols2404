f9020c8a7768c93a626c4b644a6cff8de262abb7
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-18 15:58:44 +0530

    Work on new task module Fixes #5

---
+++ web-development/front-end/ui/dashboard/dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="main-grid-container">
+    <header><?php require('header.php') ?></header>
+    <aside><?php require('sidebar.php') ?></aside>
+    <main>
+        <h3>Dashboard</h3>
+    </main>
+    <footer><?php require('footer.php') ?></footer>
+</body>
+</html>
---
+++ web-development/front-end/ui/dashboard/layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="main-grid-container">
+    <header><?php require('header.php') ?></header>
+    <aside><?php require('sidebar.php') ?></aside>
+    <main>Main</main>
+    <footer><?php require('footer.php') ?></footer>
+</body>
+</html>
